0492274331108fdf18cafb073a81e196443b7664
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2016-10-20 22:02:18 -0400

    TRACECOMPASS3.2_START: Instructions for analysis requirements exercise
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

--- org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingTimeAnalysis.java
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingTimeAnalysis.java
@@ -8,15 +8,26 @@
  *******************************************************************************/
 package org.eclipse.tracecompass.training.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.tracecompass.tmf.core.analysis.TmfAbstractAnalysisModule;
+import org.eclipse.tracecompass.tmf.core.analysis.requirements.TmfAbstractAnalysisRequirement;
 import org.eclipse.tracecompass.tmf.core.exceptions.TmfAnalysisException;
 
 public class ProcessingTimeAnalysis extends TmfAbstractAnalysisModule {
 
+    public static final String CREATE_EVENT = "ust_master:CREATE"; //$NON-NLS-1$
+    public static final String START_EVENT = "ust_master:START"; //$NON-NLS-1$
+    public static final String STOP_EVENT = "ust_master:STOP"; //$NON-NLS-1$
+    public static final String END_EVENT = "ust_master:END"; //$NON-NLS-1$
+    public static final String PROCESS_INIT_EVENT = "ust_master:PROCESS_INIT"; //$NON-NLS-1$
+    public static final String PROCESS_START_EVENT = "ust_master:PROCESS_START"; //$NON-NLS-1$
+    public static final String PROCESS_END_EVENT = "ust_master:PROCESS_END"; //$NON-NLS-1$
+
     public ProcessingTimeAnalysis() {
-        // TODO Auto-generated constructor stub
     }
 
     @Override
@@ -27,8 +38,26 @@ public class ProcessingTimeAnalysis extends TmfAbstractAnalysisModule {
 
     @Override
     protected void canceling() {
-        // TODO Auto-generated method stub
-
     }
 
+    @Override
+    public @NonNull Iterable<@NonNull TmfAbstractAnalysisRequirement> getAnalysisRequirements() {
+        Set<TmfAbstractAnalysisRequirement> requirements = new HashSet<>();
+        /*
+         * TODO:
+         * - Create a Collection with event names
+         * - Use constants above
+         */
+
+        /* TODO:
+         * - Create a TmfAnalysiEventRequirement instance
+         */
+
+        /*
+         * TODO:
+         * - Add requirements to the requirements set
+         */
+        // Return the set (which is an iterable)
+        return requirements;
+    }
 }
