0d1bc2b0b9f113ed99a993fc85ecaa6b871248c3
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2016-10-25 06:18:18 -0400

    Change to PROCESSING state of attribute Requester/<requester>
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

--- org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingTimeStateProvider.java
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingTimeStateProvider.java
@@ -25,7 +25,7 @@ public class ProcessingTimeStateProvider extends AbstractTmfStateProvider {
 
     @Override
     public int getVersion() {
-        return 1;
+        return 2;
     }
 
     @Override
@@ -53,7 +53,7 @@ public class ProcessingTimeStateProvider extends AbstractTmfStateProvider {
          *
          */
         switch (event.getName()) {
-        case IEventConstants.CREATE_EVENT:
+        case IEventConstants.CREATE_EVENT: {
             // get event field with name
             String requester = event.getContent().getFieldValue(String.class, "requester");
             if (requester == null) {
@@ -70,10 +70,26 @@ public class ProcessingTimeStateProvider extends AbstractTmfStateProvider {
             // apply state change
             stateSystem.modifyAttribute(t, stateValue, quark);
             return;
+        }
 
-        case IEventConstants.START_EVENT:
-            // TODO update state of attribute Requester/requesterString to PROCESSING
+        case IEventConstants.START_EVENT: {
+            // get event field with name
+            String requester = event.getContent().getFieldValue(String.class, "requester");
+            if (requester == null) {
+                return;
+            }
+
+            // get quark of attribute for path Requester/requesterString
+            int quark = stateSystem.getQuarkAbsoluteAndAdd("Requester", requester);
+            ITmfStateValue stateValue = TmfStateValue.newValueInt(IEventConstants.ProcessingStates.PROCESSING.ordinal());
+
+            // get time of event
+            long t = event.getTimestamp().getValue();
+
+            // apply state change
+            stateSystem.modifyAttribute(t, stateValue, quark);
             return;
+        }
 
         case IEventConstants.STOP_EVENT:
             // TODO update state of attribute Requester/requesterString to WAITING
