63ea222e088b371803049176244d8894f91f1cde
Marc-Andre Laperle <malaperle@gmail.com>
2016-11-01 09:58:47 -0400

    TRACECOMPASS2.1_ANSWERS: Class created. Put something when trace opened

---
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/EventReader.java
@@ -0,0 +1,21 @@
+package org.eclipse.tracecompass.training.example;
+
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalManager;
+import org.eclipse.tracecompass.tmf.core.signal.TmfTraceOpenedSignal;
+
+public class EventReader {
+
+    public EventReader () {
+        TmfSignalManager.register(this);
+    }
+
+    @TmfSignalHandler
+    public void traceOpened(TmfTraceOpenedSignal signal) {
+        System.out.println("trace opened: " + signal.getTrace().getName());
+    }
+
+    public void dispose() {
+        TmfSignalManager.deregister(this);
+    }
+}
